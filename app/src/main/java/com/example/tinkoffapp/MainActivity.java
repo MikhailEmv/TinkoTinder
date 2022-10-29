adbf9a4d0a9e20707145e1c6a6ac3c0695b0891e
MikhailEmv <cheburusshka0303@gmail.com>
2022-10-29 21:18:17 +0500

    Third Version

--- app/src/main/java/com/example/tinkoffapp/MainActivity.java
+++ app/src/main/java/com/example/tinkoffapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tinkoffapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
         btnSubmit = findViewById(R.id.btnSubmit);
 
         txtLoginInfo = findViewById(R.id.txtLoginInfo);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            startActivity(new Intent(MainActivity.this,
+                    FriendsActivity.class));
+            finish();
+        }
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,7 +86,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void handleLogin() {
-
+        FirebaseAuth.
+                getInstance().
+                createUserWithEmailAndPassword(edtEmail.getText().toString(),
+                        edtPassword.getText().toString()).
+                addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(MainActivity.this,
+                                    FriendsActivity.class));
+                            Toast.makeText(
+                                    MainActivity.this,
+                                    "Logged in successfully",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(
+                                    MainActivity.this,
+                                    task.getException().getLocalizedMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void handleSignUp() {
@@ -90,9 +119,17 @@ public class MainActivity extends AppCompatActivity {
              @Override
              public void onComplete(@NonNull Task<AuthResult> task) {
                  if (task.isSuccessful()) {
+                     FirebaseDatabase.getInstance().getReference("user" +
+                             FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(new User(
+                             edtUsername.getText().toString(),
+                             edtEmail.getText().toString(),
+                             ""
+                     ));
+                     startActivity(new Intent(MainActivity.this,
+                             FriendsActivity.class));
                      Toast.makeText(
                              MainActivity.this,
-                             "Logged in successfully",
+                             "Signed up successfully",
                              Toast.LENGTH_SHORT).show();
                  } else {
                      Toast.makeText(
